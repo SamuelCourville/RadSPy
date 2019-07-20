f5d8cfa68f6bba6cd470af3fcfe36a7fb30aec75
scourvil <scourvil@mymail.mines.edu>
2019-07-19 21:34:50 -0600

    initial commit

---
+++ radar_FM1D_refMethod.py
@@ -0,0 +1,312 @@
+###############################################################################
+###############################################################################
+#                       1D Radar simulator
+###############################################################################
+###############################################################################
+
+
+###############################################################################
+#                       import statements and constants
+###############################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import sys
+
+c = 2.99792*10**8     # speed of light [m/s]
+eps0 = 8.854188e-12   # vacuum permitivity [F/m or something like that]   
+mu = 4*np.pi*10**-7
+    
+    
+###############################################################################
+#                       Global Variables
+###############################################################################
+H = 0
+dB_noise = 0
+h = np.array([0])
+eps = np.array([1.0])
+lossTangent = np.array([0.0])
+nl=0
+useConductivity = 0
+windowNum = 0
+fmin = 0
+fmax = 0
+pulseFile = ""
+filterFile = ""
+
+###############################################################################
+#                       Flags
+###############################################################################
+timeFlag = False
+plotResult = True
+
+###############################################################################
+#                       Main simulation function
+###############################################################################
+def freqComponent(omega,a):
+    
+    if not useConductivity:
+        sigma = abs(omega)*eps*lossTangent
+    else:
+        sigma = lossTangent
+    
+    M = int(nl)
+    K = np.zeros((M+1),dtype=complex)
+    Z = np.zeros((M),dtype=complex)
+    u = np.zeros((M+1),dtype=complex)
+    gamma2 = np.zeros((M+1),dtype=complex)
+    
+    if omega==0:
+        omega = 0.000000001
+
+    lamb=0
+    for i in range(0,M+1):
+        gamma2[i] = 1j*sigma[i]*mu*omega-eps[i]*mu*omega**2
+        u[i] = np.sqrt(lamb**2+gamma2[i])
+        K[i] = u[i]/(sigma[i]+1j*omega*eps[i])
+
+        
+    if M>1:
+        indexes = np.flipud(np.linspace(1,M-2,M-2,dtype=int))
+        
+        
+        Z[-1] = K[M-1]*(K[M]+K[M-1]*np.tanh(u[M-1]*h[M-1]))/(K[M-1]+K[M]*np.tanh(u[M-1]*h[M-1]))
+        for i in indexes:
+            Z[i] = K[i]*(Z[i+1]+K[i]*np.tanh(u[i]*h[i]))/(K[i]+Z[i+1]*np.tanh(u[i]*h[i]))
+    else:
+        Z = np.zeros((M+1),dtype=complex)
+        Z[1] = K[M-1]*(K[M]+K[M-1]*np.tanh(u[M-1]*h[M-1]))/(K[M-1]+K[M]*np.tanh(u[M-1]*h[M-1]))
+    
+    a = a/Z[1]
+    b = a*(K[0]-Z[1])/(K[0]+Z[1])
+    b = Z[1]*b
+    return b
+
+
+###############################################################################
+#                       File loading functions
+###############################################################################
+def loadModel(fileName):
+    global H
+    global dB_noise
+    global h
+    global eps
+    global lossTangent
+    global nl
+    global useConductivity
+    global windowNum
+    global fmin
+    global fmax
+    global pulseFile
+    global filterFile
+    with open(fileName) as f:
+        junk = f.readline()
+        junk = f.readline()
+        H = float(f.readline().strip())                 # Spacecraft Altitude (m)
+        junk = f.readline()
+        dB_noise = float((f.readline().strip()))          # What's the noise level in dB?
+        dB_noise = dB_noise-100/dB_noise                # This is an approximation. Ask me for expln. 
+        junk = f.readline()
+        nl = float((f.readline().strip()))                # number of layers
+        junk = f.readline()
+        depthsstr = (f.readline().strip())
+        depthsstr = depthsstr.split(' ')
+        depths = [float(i) for i in depthsstr]         # Layer Thicknesses (m)
+        junk = f.readline()
+        epsrstr = (f.readline().strip())
+        epsrstr = epsrstr.split(' ')
+        epsr = [float(i) for i in epsrstr]             # real relative permitivity
+        junk = f.readline()
+        useConductivity = float((f.readline().strip()))   # number of layers
+        junk = f.readline()
+        lossTangentstr = (f.readline().strip())           # loss tangent or conductivity
+        lossTangentstr = lossTangentstr.split(' ')
+        lossTangentPre = [float(i) for i in lossTangentstr]
+        junk = f.readline()
+        pulseFile = f.readline().strip()
+        junk = f.readline()
+        filterFile = f.readline().strip()
+        junk = f.readline()
+        windowNum = float((f.readline().strip()))
+        junk = f.readline()
+        windowFreqs = (f.readline().strip()) 
+        windowFreqs = windowFreqs.split(' ')
+        windowFreqs = [float(i) for i in windowFreqs]
+        
+        fmin = windowFreqs[0]
+        fmax = windowFreqs[1]
+        
+        epsr = np.array(epsr)
+        h = np.append(h, np.array(depths))
+        eps = np.append(eps,epsr)*8.85e-12
+        lossTangent = np.append(lossTangent,np.array(lossTangentPre))
+    
+    
+def loadTruePulse(fileName):
+    with open(fileName) as csvfile:
+        readCSV = csv.reader(csvfile,delimiter=',')
+        t = []
+        E = []
+        for row in readCSV:
+            t.append(np.real(complex(row[0])))
+            E.append(complex(row[1]))
+    return t,E    
+    
+def loadMatchFilter(fileName):
+    with open(fileName) as csvfile:
+        readCSV = csv.reader(csvfile,delimiter=',')
+        t = []
+        f = []
+        E = []
+        for row in readCSV:
+            t.append(np.real(complex(row[0])))
+            f.append(np.real(complex(row[1])))
+            E.append(complex(row[2]))
+    return t,f,E
+
+###############################################################################
+#                       Range Compression and windowing
+###############################################################################
+def rangeCompress(data, mfilter, f):
+    out = data*mfilter
+    if windowNum!=0:
+        i1 = np.argmin(abs(f-fmin))
+        i2 = np.argmin(abs(f-fmax))
+        out[0:i1] = out[0:i1]*0
+        out[i1:i2] = np.kaiser(i2-i1,windowNum)*out[i1:i2]
+        out[i2:-1] = out[i2:-1]*0
+    out = np.fft.ifft(np.fft.ifftshift(out))*np.sqrt(len(data))
+    return out
+
+def interpFreq(t2,t1,Fnew):
+    temp = np.fft.ifft(np.fft.ifftshift(Fnew))
+    return t2, np.fft.fftshift(np.fft.fft(np.interp(t2,t1,temp,left=0,right=0)))
+    
+def dB(x):
+    return 20*np.log10(x)
+    
+###############################################################################
+#                       Main script
+###############################################################################
+def main(args):
+    
+    if len(args) != 2:
+        print("Please provide 1 model input file name (txt)")
+        quit()
+    else: 
+        modelFile = args[1]
+    
+    
+    loadModel(modelFile)
+    modelFile = modelFile[0:-4]
+    
+    t,source = loadTruePulse(pulseFile)
+    
+
+    n=len(t)
+    d = t[1]-t[0]
+
+    F = np.fft.fftshift(np.fft.fft(source))/np.sqrt(n)
+    Ffreq = np.fft.fftshift(np.fft.fftfreq(n,d))
+
+
+    Fnew = np.zeros((n),dtype=complex)
+    for i in range(0,n):
+        Fnew[i] = freqComponent(abs(2*np.pi*Ffreq[i]),F[i])
+
+    
+
+    if dB_noise>-100:
+        Prx = np.max(Fnew)**2
+        Pnoise = Prx*10**(dB_noise/10)*np.sqrt(n)
+        noise = np.sqrt(Pnoise)*(2*np.random.rand((n))-1 + 1j*(2*np.random.rand((n))-1))
+        Fnew = Fnew+noise
+    
+    
+    
+    Aillum = (H**2)*np.pi
+    sigma0 = (fmax-fmin)*(np.max(t)-np.min(t))
+    lam = c/((fmin+fmax)/2)
+    Prx = sigma0*Aillum*(lam**2)/(64*(H**4)*np.pi**3)
+    Fnew = Fnew*np.sqrt(Prx)
+    
+    
+    if filterFile != "none":
+        tmf,f,matchFilter = loadMatchFilter(filterFile)
+        if (f!= Ffreq).any():
+            t, Fnew = interpFreq(tmf,t,Fnew)
+        result = rangeCompress(Fnew,matchFilter,np.array(f))
+    else:
+        result = np.fft.ifft(np.fft.ifftshift(Fnew))*np.sqrt(n)
+    result = np.fft.ifftshift(result)
+    
+
+    
+    addTime=0
+    if timeFlag:
+        addTime = 2*H/c
+        
+    finalData = dB(result)-np.max(dB(result))
+    a = np.transpose(np.asarray([np.array(t)+addTime, finalData, result]))
+    np.savetxt(modelFile + "_output_data.csv", a, delimiter=",")
+
+    
+    if plotResult:
+        plotDataModel(addTime,np.array(t),result,modelFile)
+    
+    return 
+        
+    
+###############################################################################
+#                       plot function
+###############################################################################
+def plotDataModel(addTime,finalT,sout,modelFile):
+    dB_limit = -50
+    endT = 6
+    
+    fig=plt.figure(figsize=(9, 8), dpi= 80, facecolor='w', edgecolor='k')
+
+    plt.subplot(1,2,1)
+    plt.plot(dB(sout)-np.max(dB(sout)),(finalT+addTime)*10**6,linewidth=3)
+    plt.grid()
+    axes = plt.gca()
+    axes.set_ylim([0+addTime*10**6,endT+addTime*10**6])
+    axes.set_xlim([dB_limit,0])
+    axes.set_ylabel('Time ($\mu$s)',fontsize=16)
+    axes.set_xlabel('dB',fontsize=16)
+    axes.set_title('Radar return',fontsize=20)
+    axes.invert_yaxis()
+
+
+    layers = np.append(np.cumsum(h),[0])
+    nz = 200
+    z = np.linspace(0,np.max(layers)+20,nz)
+    currInd = -1
+    epsz = np.zeros((nz))
+    for i in range(0,nz):
+        if z[i] < layers[currInd]:
+            epsz[i] = eps[currInd]/eps0
+        elif currInd<nl:
+            currInd = currInd+1
+            epsz[i] = eps[currInd]/eps0
+        else:
+            epsz[i] = eps[currInd]/eps0
+        
+    
+    plt.subplot(1,2,2)
+    plt.plot(epsz,z,'r',linewidth=4.0)
+    axes = plt.gca()
+    axes.set_ylabel('Depth (m)',fontsize=16)
+    axes.set_xlabel('$\epsilon_r$',fontsize=16)
+    axes.set_ylim([-0,np.max(z)])
+    axes.set_xlim([0,np.max(epsz)+1])
+    axes.set_title('Permitivity Model',fontsize=20)
+    axes.invert_yaxis()
+
+    plt.savefig(modelFile + '_output_figure.png')
+
+    b = np.transpose(np.asarray([ z,epsz]))
+    np.savetxt(modelFile + "_model.csv", b, delimiter=",")
+    
+main(sys.argv)
